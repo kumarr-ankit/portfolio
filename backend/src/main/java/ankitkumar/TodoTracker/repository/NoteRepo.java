8120292e8cf7df248a53d05ec44499d551dfc43c
Ankit Kumar <ankitkr5447@gmail.com>
2025-10-30 17:54:21 +0530

    Delete backend directory

--- backend/src/main/java/ankitkumar/TodoTracker/repository/NoteRepo.java
+++
@@ -1,9 +0,0 @@
-package ankitkumar.TodoTracker.repository;
-
-import ankitkumar.TodoTracker.entity.NoteEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoteRepo extends JpaRepository<NoteEntity,Long> {
-}
