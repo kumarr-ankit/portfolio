2e589a5e2465ebc1af3ee574ae69edd8e7368ae8
Ankit Kumar <ankitkr5447@gmail.com>
2025-11-08 14:14:14 +0530

    added hero

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        ankit: ['Prosto One',, 'sans-serif'],
+      },
+    },
+  },
+};
